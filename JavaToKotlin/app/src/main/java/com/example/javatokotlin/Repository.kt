39130e13c2a63cf0cf18b5e3976970d392848843
Daniel Acosta <dmacosta@noctrl.edu>
2022-01-20 14:56:54 -0600

    Java to Kotlin file

---
+++ JavaToKotlin/app/src/main/java/com/example/javatokotlin/Repository.kt
@@ -0,0 +1,28 @@
+package com.example.javatokotlin
+
+import com.example.javatokotlin.User.lastName
+import com.example.javatokotlin.User.firstName
+import java.util.ArrayList
+
+val User.formattedName: String
+    get() {
+        return if (lastName != null) {
+            if (firstName != null) {
+                "$firstName $lastName"
+            } else {
+                lastName ?: "Unknown"
+            }
+        } else {
+            firstName ?: "Unknown"
+        }
+    }
+
+object Repository {
+
+    private val _users = mutableListOf(User("Jane", ""), User("John", null), User("Anne", "Doe"))
+    val users: List<User>
+        get() = _users
+
+    val formattedUserNames: List<String>
+        get() = _users.map { user -> user.formattedName }
+}
